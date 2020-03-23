df800edb5927f0def5758885d71c30766b0a3086
Ramon Hubbell <Ramon.hubbell@gmail.com>
2020-03-23 16:23:24 -0500

    add tests and ability to say 'hello' to any input

--- code.js
+++ code.js
@@ -3,10 +3,16 @@
     return "Hello, World!";
 }
 
+// function sayHello (input) {
+//     if (input === "Alex") {
+//         return "Hello, Alex!";
+//     } else if (input === "Pat") {
+//         return "Hello, Pat!"
+//     } else {
+//         return "Hello, Jane!";
+//     }
+// }
+
 function sayHello (input) {
-    if (input === "Alex") {
-        return "Hello, Alex!";
-    } else {
-        return "Hello, Jane!";
-    }
+    return "Hello, " + input + "!";
 }
