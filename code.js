a6fa7119d338ba551e20b87595689c63ebff7266
Ramon Hubbell <Ramon.hubbell@gmail.com>
2020-03-23 16:01:00 -0500

    add the fourth test and solution for intro-to-testing

--- code.js
+++ code.js
@@ -3,6 +3,10 @@
     return "Hello, World!";
 }
 
-function sayHello () {
-    return "Hello, Jane!";
+function sayHello (input) {
+    if (input === "Alex") {
+        return "Hello, Alex!";
+    } else {
+        return "Hello, Jane!";
+    }
 }
